20cec267ad417cab9cb3ed5ff67172fcd347060e
Terence <tjsaverin98@live.com>
2025-05-08 08:56:11 -0500

    Displayed price using javascript

--- books.js
+++ books.js
@@ -4,10 +4,10 @@ function renderBooks(filter) {
   const books = getBooks();
 
   if (filter === 'LOW_TO_HIGH') {
-    books.sort((a, b) => a.originalPrice - b.originalPrice);
+    books.sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice));
   }
   else if (filter === 'HIGH_TO_LOW') {
-    books.sort((a, b) => b.originalPrice - a.originalPrice)
+    books.sort((a, b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice))
   }
   else if ( filter === 'RATING') {
     books.sort((a, b) => b.rating - a.rating);
@@ -26,13 +26,21 @@ function renderBooks(filter) {
      ${ratingsHTML(book.rating)}
     </div>
     <div class="book__price">
-      <span>$${book.originalPrice.toFixed(2)}</span>
+      ${priceHTML(book.originalPrice, book.salePrice)}
     </div>
   </div>`;
     })
     .join("");
 
   booksWrapper.innerHTML = booksHtml;
+}
+
+function priceHTML(originalPrice, salePrice) {
+  if (!salePrice) {
+    return `$${originalPrice.toFixed(2)}`
+  }
+  return `<span class="book__price--normal">$${originalPrice}</span> $${salePrice.toFixed(2)}`
+  
 }
 
 function ratingsHTML(rating) {
