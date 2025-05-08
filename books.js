f0c6c9fb9c3b9f0bf0ef56185e5d3318be6996b0
Terence <tjsaverin98@live.com>
2025-05-08 09:55:26 -0500

    Loading State

--- books.js
+++ books.js
@@ -1,7 +1,15 @@
-function renderBooks(filter) {
-  const booksWrapper = document.querySelector(".books");
+ let books;
 
-  const books = getBooks();
+ async function renderBooks(filter) {
+  const booksWrapper = document.querySelector(".books");
+  
+  booksWrapper.classList += ' books__loading'
+
+  if (!books) {
+    books = await getBooks();
+  }
+
+  booksWrapper.classList.remove('books__loading')
 
   if (filter === 'LOW_TO_HIGH') {
     books.sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice));
